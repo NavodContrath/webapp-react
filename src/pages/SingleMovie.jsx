5ced2baf407cca723a54ddab845c610c0e764cda
NavodContrath <navod.contrath@gmail.com>
2025-04-17 01:11:40 +0200

    refactor SingleMovie component; reorganize imports and enhance movie data handling

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
 import MovieDisplayer from "../components/MovieDisplayer"
 import ReviewDisplayer from "../components/ReviewDisplayer"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useGlobal } from "../contexts/GlobalContext";
 
 export default function SingleMovie() {
+    const { movies } = useGlobal()
+    const rating = [1, 2, 3, 4, 5]
     const { id } = useParams()
     const [movie, setMovie] = useState([])
-    const rating = [1, 2, 3, 4, 5]
+
     useEffect(() => {
-        fetch('http://localhost:3000/api/v1/movies/' + id)
+        fetch(`http://localhost:3000/api/v1/movies/${id}`)
             .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                setMovie(data)
-            })
-            .then(error => console.log(error))
-    }, [])
+            .then(data => setMovie(data))
+            .catch(error => console.error(error))
+    }, [id]);
+
     return (
         <>
-            <MovieDisplayer movie={movie} />
+            <MovieDisplayer movie={movie} movies={movies} />
             <ReviewDisplayer movie={movie} rating={rating} />
         </>
     )
