370ff2ce7af7ae9b6cd27026d11c6d2adb63dc10
NavodContrath <navod.contrath@gmail.com>
2025-04-16 20:38:27 +0200

    refactor Footer component for improved layout and accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,37 @@
 export default function Footer() {
     return (
-        <footer>
-            <div className="boottom-bar bg-danger my-5">
-                <div className="container py-3">
-                    <div className="row">
-                        <div className="col-4">
-                            <div className="logo">CIAO</div>
-                        </div>
-                        <div className="col-2">
-                            <ul>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-2">
-                            <ul>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-2">
-                            <ul>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-2">
-                            <ul>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                                <li><a href="">LOREM</a></li>
-                            </ul>
-                        </div>
+        <footer className="bg-danger text-dark mt-auto py-4">
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <h5 className="fw-bold">Boolflix</h5>
                     </div>
-
+                    <div className="col-2">
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-white text-decoration-none">About</a></li>
+                            <li><a href="#" className="text-white text-decoration-none">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-2">
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-white text-decoration-none">Help Center</a></li>
+                            <li><a href="#" className="text-white text-decoration-none">Privacy</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-2">
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-white text-decoration-none">Terms</a></li>
+                            <li><a href="#" className="text-white text-decoration-none">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-2">
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-white text-decoration-none">Languages</a></li>
+                            <li><a href="#" className="text-white text-decoration-none">Cookie Preferences</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
-
     )
 }
