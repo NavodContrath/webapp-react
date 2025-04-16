30b88ba6d618e7033f7db92d0fff175c6db445fc
NavodContrath <navod.contrath@gmail.com>
2025-04-17 01:11:34 +0200

    refactor MovieDisplayer component; enhance layout and navigation links for improved user experience

--- src/components/MovieDisplayer.jsx
+++ src/components/MovieDisplayer.jsx
@@ -1,7 +1,10 @@
-export default function MovieDisplayer({ movie }) {
+import { Link } from "react-router-dom";
+
+
+export default function MovieDisplayer({ movie, movies }) {
 
     return (
-        <div className="single-movie p-5 my-4">
+        <div className="single-movie p-5 my-5">
             <div className="container bg-black text-white rounded-3 p-5 shadow">
                 <div className="row align-items-center">
                     <div className="col-md-6 mb-4 mb-md-0">
@@ -10,11 +13,23 @@ export default function MovieDisplayer({ movie }) {
                         <p className="text-dark-danger">{movie.genre}</p>
                         <p className="text-light">{movie.director}</p>
                     </div>
-                    <div className="col-md-6 text-center">
+                    <div className="col-md-6 d-flex justify-content-center gap-3 align-items-center">
+                        {movie.id > 1 && (
+                            <Link to={`/movies/${movie.id - 1}`} className="btn btn-black">
+                                <i className="bi bi-caret-left fs-3 text-dark-danger"></i>
+                            </Link>
+                        )}
                         <img className="rounded shadow-sm" height={'500px'} src={`http://localhost:3000/images/${movie.image}`} alt={movie.title} />
+                        {
+                            movie.id < movies.length && (
+                                <Link to={`/movies/${movie.id + 1}`} type="button" className="btn  btn-black">
+                                    <i className="bi bi-caret-right fs-3 text-dark-danger"></i>
+                                </Link>
+                            )
+                        }
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
