64345a452bf4ca7019e69151323e4e66844a14d1
NavodContrath <navod.contrath@gmail.com>
2025-04-16 20:21:27 +0200

    add route for SingleMovie component to display movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layout/DefaultLayout'
 import HomePage from './pages/HomePage'
 import { GlobalProvider } from './contexts/GlobalContext'
+import SingleMovie from './pages/SingleMovie'
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' Component={DefaultLayout}>
               <Route path='/' Component={HomePage} />
+              <Route path='/movies/:id' Component={SingleMovie} />
             </Route>
           </Routes>
         </BrowserRouter>
