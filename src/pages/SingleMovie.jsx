d94bb5e85f4630ebbe042786a5c1dd8a8e3e25e8
NavodContrath <navod.contrath@gmail.com>
2025-04-16 21:04:15 +0200

    refactor SingleMovie component to enhance review layout and improve responsiveness

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -26,36 +26,41 @@ export default function SingleMovie() {
                             <p className="text-light">{movie.director}</p>
                         </div>
                         <div className="col-md-6 text-center">
-                            <img className="rounded shadow-sm img-fluid" height={'500px'} src={`http://localhost:3000/images/${movie.image}`} alt={movie.title} />
+                            <img className="rounded shadow-sm" height={'500px'} src={`http://localhost:3000/images/${movie.image}`} alt={movie.title} />
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="container">
-                {
-                    movie.reviews?.map(review => {
-                        return (
-                            <div className="card bg-dark text-white mb-4 shadow rounded-4 border border-secondary" key={review.id}>
-                                <div className="card-header d-flex align-items-center justify-content-between border-bottom border-secondary">
-                                    <h5 className="text-danger fw-bold mb-0">{review.name}</h5>
-                                    <div className="rating d-flex">
-                                        {
-                                            rating.map((star) => (
-                                                <i key={star} className={`fa-star me-1 ${star <= Math.ceil(review.vote) ? 'fa-solid text-warning' : 'fa-regular text-secondary'}`}></i>
-                                            ))
-                                        }
+            <div className="container mb-5">
+                <h2 className="text-white bg-dark m-0 rounded-top-3 p-2 border border-secondary">Reviews</h2>
+                <div className="row py-3">
+                    {
+                        movie.reviews?.map(review => {
+                            return (
+                                <div className="col-lg-6 col-sm-12" key={review.id}>
+                                    <div className="card bg-dark text-white mb-4 shadow rounded-4 border border-secondary" key={review.id}>
+                                        <div className="card-header d-flex align-items-center justify-content-between border-bottom border-secondary">
+                                            <h5 className="text-danger fw-bold mb-0">{review.name}</h5>
+                                            <div className="rating d-flex">
+                                                {
+                                                    rating.map((star) => (
+                                                        <i key={star} className={`fa-star me-1 ${star <= Math.ceil(review.vote) ? 'fa-solid text-warning' : 'fa-regular text-secondary'}`}></i>
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-light">{review.text}</p>
+                                        </div>
+                                        <div className="card-footer border-top border-secondary">
+                                            Created at: {review.created_at.slice(0, 10)}
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="card-body">
-                                    <p className="card-text text-light">{review.text}</p>
-                                </div>
-                                <div className="card-footer border-top border-secondary">
-                                    Created at: {review.created_at.slice(0, 10)}
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
         </>
     )
