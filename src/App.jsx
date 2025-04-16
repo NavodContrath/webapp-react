e1787ad15d52586d170f048ade51fae67c7ad0a2
NavodContrath <navod.contrath@gmail.com>
2025-04-16 19:13:28 +0200

    wrap App component with GlobalProvider to manage global state

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layout/DefaultLayout'
 import HomePage from './pages/HomePage'
+import { GlobalProvider } from './contexts/GlobalContext'
 
 function App() {
 
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' Component={DefaultLayout}>
-            <Route path='/' Component={HomePage} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' Component={DefaultLayout}>
+              <Route path='/' Component={HomePage} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
 
     </>
   )
