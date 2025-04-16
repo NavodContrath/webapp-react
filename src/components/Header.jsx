e33f14dba475b731ca4dd7df1cbc0d1cdbb7c414
NavodContrath <navod.contrath@gmail.com>
2025-04-16 21:04:11 +0200

    refactor Header component to enhance layout and improve search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,38 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 
 export default function Header() {
+    const location = useLocation()
+    const isSingleMoviePage = location.pathname.startsWith("/movies/")
     return (
         <header>
-            <div className="nav-bar py-3 bg-danger">
-                <div className="container-fluid d-flex flex-wrap justify-content-between align-items-center px-5">
-                    <div className="d-flex align-items-center gap-4">
-                        <div className="logo fw-bold fs-4 text-dark">
-                            <NavLink className='nav-link text-white' to={'/'}>Boolflix</NavLink>
-                        </div>
+            {isSingleMoviePage ? (
+                <div className="nav-bar  bg-danger">
+                    <div className="container d-flex justify-content-center align-items-center" style={{ height: "60px" }}>
+                        <NavLink className="nav-link fw-bold fs-3 text-dark" to="/">Boolflix</NavLink>
                     </div>
-                    <form className="d-flex mt-3 mt-md-0" role="search">
-                        <input type="search" className="form-control bg-dark text-white border-0 rounded-0" placeholder="Search" />
-                        <button type="submit" className="btn btn-dark rounded-0 border-0">
-                            <i className="bi bi-search"></i>
-                        </button>
-                    </form>
                 </div>
-            </div>
+            ) : (
+                <div className="nav-bar py-3 bg-danger">
+                    <div className="container-fluid d-flex flex-wrap justify-content-between align-items-center px-5">
+                        <div className="d-flex align-items-center gap-4">
+                            <div className="logo fw-bold fs-4">
+                                <NavLink className="nav-link text-dark" to="/">Boolflix</NavLink>
+                            </div>
+                        </div>
+                        <form className="d-flex mt-3 mt-md-0" role="search">
+                            <input
+                                type="search"
+                                className="form-control bg-dark text-white border-0 rounded-0"
+                                placeholder="Search"
+                            />
+                            <button type="submit" className="btn btn-dark rounded-0 border-0">
+                                <i className="bi bi-search"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
+
         </header>
 
     )
