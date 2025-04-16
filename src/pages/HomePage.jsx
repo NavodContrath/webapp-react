db4f4338e7e30cb1ee04fd1f60f56aa3b1104f0f
NavodContrath <navod.contrath@gmail.com>
2025-04-16 20:22:00 +0200

    refactor HomePage component for improved styling and structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { useGlobal } from "../contexts/GlobalContext"
 export default function HomePage() {
     const { movies } = useGlobal()
@@ -5,10 +6,12 @@ export default function HomePage() {
     return (
         <>
             <div className="p-5 mb-4">
-                <div className="container-fluid py-5 bg-danger">
-                    <h1 className="display-5 fw-bold">Welcome to Boolflix!</h1>
-                    <p className="col-md-8 fs-4">
-                        The Ohara Library was the largest and most important center of knowledge, located on the island of Ohara in the West Blue. It was home to the Tree of Knowledge, a massive tree housing vast archives of historical texts, research, and ancient documents.
+                <div className="container py-5 bg-dark text-white rounded-4 shadow border border-light">
+                    <h1 className="display-4 fw-bold mb-3">Welcome to <span className="text-danger">Boolflix</span>!</h1>
+                    <p className="fs-5 lh-lg">
+                        Boolflix is the world's leading streaming entertainment service with over 200 million paid memberships in over 190 countries. Members enjoy a wide variety of award-winning TV shows, movies, documentaries, and more—anytime, anywhere, and on any internet-connected screen.
+                        <br /><br />
+                        With Boolflix, you're in control: pause, play, resume watching, or binge an entire season—there are no commercials, and there's always something new to discover.
                     </p>
                 </div>
             </div>
@@ -18,11 +21,11 @@ export default function HomePage() {
                         movies.map(movie => {
                             return (
                                 <div className="col" key={movie.id}>
-                                    <div className="card border-secondary h-100">
-                                        <img className="card-img-top" src={`http://localhost:3000/images/${movie.image}`} alt={movie.title} />
+                                    <div className="card border-secondary bg-dark text-white h-100">
+                                        <Link to={`/movies/${movie.id}`}><img className="card-img-top" src={`http://localhost:3000/images/${movie.image}`} alt={movie.title} /></Link>
                                         <div className="card-body">
-                                            <h5 className="card-title">{movie.title}</h5>
-                                            <p className="card-text text-muted">{movie.abstract}</p>
+                                            <h5 className="card-title text-danger fs-4 fw-bold">{movie.title}</h5>
+                                            <p className="card-text small fst-italic">{movie.abstract}</p>
                                         </div>
                                     </div>
                                 </div>
