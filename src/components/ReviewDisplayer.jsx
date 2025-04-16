c9e8c73d2ebf64cd04e949d34659fce8ad820fe4
NavodContrath <navod.contrath@gmail.com>
2025-04-16 22:17:41 +0200

    add ReviewDisplayer component to display movie reviews with ratings

---
+++ src/components/ReviewDisplayer.jsx
@@ -0,0 +1,38 @@
+export default function ReviewDisplayer({ movie, rating }) {
+    return (
+        <>
+            <div className="container mb-5 p-0  bg-black  rounded-3">
+                <h2 className="text-dark-danger m-0  p-3 border-light border-bottom">Reviews</h2>
+                <div className="p-3">
+                    {
+                        movie.reviews?.map(review => {
+                            return (
+                                <div className="col-12" key={review.id}>
+                                    <div className="card bg-black text-white mb-4 border border-secondary " key={review.id}>
+                                        <div className="card-header d-flex align-items-center justify-content-between">
+                                            <h5 className="text-white fw-bold mb-0">{review.name}</h5>
+                                            <div className="rating d-flex">
+                                                {
+                                                    rating.map((star) => (
+                                                        <i key={star} className={`fa-star me-1 ${star <= Math.ceil(review.vote) ? 'fa-solid text-warning' : 'fa-regular text-secondary'}`}></i>
+                                                    ))
+                                                }
+                                            </div>
+                                        </div>
+                                        <div className="card-body">
+                                            <p className="card-text text-light">{review.text}</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            Created at: {review.created_at.slice(0, 10)}
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+        </>
+    )
+}
