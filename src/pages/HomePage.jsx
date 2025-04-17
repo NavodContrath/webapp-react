0f8534f4ff3679395383fd5bdf4a91d412085839
NavodContrath <navod.contrath@gmail.com>
2025-04-17 02:48:12 +0200

    fix: add horizontal padding to container in HomePage for improved layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     return (
         <>
             <div className="p-5 mt-3">
-                <div className="container py-5 bg-black text-white rounded-4 shadow ">
+                <div className="container py-5 px-4 bg-black text-white rounded-4 shadow ">
                     <h1 className="display-4 fw-bold mb-3">Welcome to <span className="text-dark-danger">Boolflix</span>!</h1>
                     <p className="fs-5 lh-lg">
                         Boolflix is the world's leading reviewing service with over 2000+ movies reviewed. Members enjoy reviews for a wide variety of award-winning TV shows, movies, documentaries.
