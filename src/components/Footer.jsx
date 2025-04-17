929c6f74bc75eb47dbd541adb6a074a204660a86
NavodContrath <navod.contrath@gmail.com>
2025-04-17 02:27:27 +0200

    refactor Footer component; conditionally render footer based on the current page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,53 @@
+import { useLocation } from "react-router-dom"
 export default function Footer() {
+    const location = useLocation()
+    const isFormPage = location.pathname.startsWith("/reviews/add_review")
+
     return (
-        <footer className="mt-auto py-4">
-            <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                        <h5 className="fw-bold">Boolflix</h5>
-                    </div>
-                    <div className="col-2">
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="text-white text-decoration-none">About</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Careers</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-2">
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="text-white text-decoration-none">Help Center</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Privacy</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-2">
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="text-white text-decoration-none">Terms</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-2">
-                        <ul className="list-unstyled">
-                            <li><a href="#" className="text-white text-decoration-none">Languages</a></li>
-                            <li><a href="#" className="text-white text-decoration-none">Cookie Preferences</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <>
+            {
+                !isFormPage ? (
+
+                    <footer className="mt-auto py-4">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-4">
+                                    <h5 className="fw-bold">Boolflix</h5>
+                                </div>
+                                <div className="col-2">
+                                    <ul className="list-unstyled">
+                                        <li><a href="#" className="text-white text-decoration-none">About</a></li>
+                                        <li><a href="#" className="text-white text-decoration-none">Careers</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-2">
+                                    <ul className="list-unstyled">
+                                        <li><a href="#" className="text-white text-decoration-none">Help Center</a></li>
+                                        <li><a href="#" className="text-white text-decoration-none">Privacy</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-2">
+                                    <ul className="list-unstyled">
+                                        <li><a href="#" className="text-white text-decoration-none">Terms</a></li>
+                                        <li><a href="#" className="text-white text-decoration-none">Contact</a></li>
+                                    </ul>
+                                </div>
+                                <div className="col-2">
+                                    <ul className="list-unstyled">
+                                        <li><a href="#" className="text-white text-decoration-none">Languages</a></li>
+                                        <li><a href="#" className="text-white text-decoration-none">Cookie Preferences</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                ) : (
+                    <footer className="footer mt-auto py-3 bg-black text-center text-white">
+                        <div className="container">
+                            <small className="text-white">&copy; 2025 Boolflix Movie Reviews Inc.</small>
+                        </div>
+                    </footer>)
+            }
+        </>
     )
 }
