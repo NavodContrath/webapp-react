2d08b6767340c13bc59149271e36f4f9246f32a0
NavodContrath <navod.contrath@gmail.com>
2025-04-23 18:32:18 +0200

    feat: add loading state to SingleMovie component for improved user experience during data fetching

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { useGlobal } from "../contexts/GlobalContext";
 
 export default function SingleMovie() {
-    const { movies } = useGlobal()
+    const { movies, loading, setLoading } = useGlobal()
     const rating = [1, 2, 3, 4, 5]
     const { id } = useParams()
     const [movie, setMovie] = useState([])
@@ -13,14 +13,17 @@ export default function SingleMovie() {
     useEffect(() => {
         fetch(`http://localhost:3000/api/v1/movies/${id}`)
             .then(response => response.json())
-            .then(data => setMovie(data))
+            .then(data => {
+                setMovie(data)
+                setLoading(false)
+            })
             .catch(error => console.error(error))
     }, [id]);
 
     return (
         <>
-            <MovieDisplayer movie={movie} movies={movies} />
-            <ReviewDisplayer movie={movie} rating={rating} />
+            <MovieDisplayer movie={movie} movies={movies} loading={loading} />
+            <ReviewDisplayer movie={movie} rating={rating} loading={loading} />
         </>
     )
 }
