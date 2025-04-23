46cce2580305ad03e17ff0006a31c0f1cdfbbfe1
NavodContrath <navod.contrath@gmail.com>
2025-04-23 18:32:22 +0200

    feat: add loading state to MovieDisplayer component for improved user experience during data fetching

--- src/components/MovieDisplayer.jsx
+++ src/components/MovieDisplayer.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
 
-export default function MovieDisplayer({ movie, movies }) {
+export default function MovieDisplayer({ movie, movies, loading }) {
 
     return (
-        <div className="single-movie p-5 mb-5">
+        <div className="single-movie p-5 mb-5 position-relative">
+            {loading && (
+                <div className="d-flex justify-content-center align-items-center loader">
+                    <div className="spinner-border text-light" style={{ width: "4rem", height: "4rem" }} role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            )}
             <div className="container bg-black text-white rounded-3 p-5 shadow">
                 <div className="row align-items-center">
                     <div className="col-md-6 mb-4 mb-md-0">
